ccd20fde790e579ffde9d3fd53e0f2e292e486cf
ambijani <a.bijani@wustl.edu>
2023-07-22 17:23:30 -0700

    averaged all angles together indv

---
+++ lost.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+import argparse
+    
+parser = argparse.ArgumentParser(
+    prog='Polar Direction Plotter',
+    description='Plots slow-wave direction data on a polar histogram',
+    epilog='Hope this works')
+parser.add_argument('filenames', nargs='+', help='input wavefronts_direction_local')
+parser.add_argument('--wave_ids', type=int, metavar='N', nargs='*', action='append', help='input desired wavefronts')
+args = parser.parse_args()
+
+all_directionY = []
+all_directionX = []
+lst = []
+all_weights = []     
+df_len = 0
+
+for i, filename in enumerate(args.filenames):
+    df = pd.read_csv(Path(f"D:\\ANESTH_{filename}_KX_16\\stage05_channel-wave_characterization\\direction_local\\wavefronts_direction_local.csv"))
+
+    wave_ids = args.wave_ids[i] if args.wave_ids else df['wavefronts_id'].unique()
+   
+    wave_id = ' '.join(map(str, wave_ids))
+    last_number = df['wavefronts_id'].iloc[-1]
+
+    for wave_id in wave_ids:
+        group = df[df['wavefronts_id'] == wave_id]
+
+        directionY = group['direction_local_y'].tolist()
+        directionX = group['direction_local_x'].tolist()
+
+        avg_y = np.average(directionY)
+        avg_x = np.average(directionX)
+
+        data = {avg_x, avg_y}
+        lst.append(data)
+        
+        all_directionY.extend(directionY)
+        all_directionX.extend(directionX)
+
+
+avg_df = pd.DataFrame(lst, columns=['avg_x', 'avg_y'], index = wave_ids)
+
+# Calculate angles using arctan2
+angles = np.arctan2(all_directionY, all_directionX)
+angles_w_avg = np.arctan2(avg_df['avg_y'], avg_df['avg_x'])
+magnitudes = np.linalg.norm(np.vstack([all_directionX, all_directionY]), axis=0)
+magnitudes1 = np.linalg.norm(np.vstack([avg_df['avg_x'], avg_df['avg_y']]), axis=0)
+
+# Calculate the weighted average angle
+weighted_average_angle = np.arctan2(np.average(all_directionY, weights=magnitudes, axis=0), 
+                                    np.average(all_directionX, weights=magnitudes, axis=0))
+weighted_average_angle1 = np.arctan2(np.average(avg_df['avg_y'], weights=magnitudes1, axis=0), 
+                                    np.average(avg_df['avg_x'], weights=magnitudes1, axis=0))
+print('weighted with all vectors together', weighted_average_angle)
+print('weighted with each wave_id\'s average then averaged together', weighted_average_angle1)
+
+# Create a polar histogram
+fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+ax.hist(angles, bins=36, range=(-np.pi, np.pi), density=True, weights=magnitudes)
+
+fig1, ax1 = plt.subplots(subplot_kw={'projection': 'polar'})
+ax1.hist(angles_w_avg, bins=36, range=(-np.pi, np.pi), density=True, weights=magnitudes1)
+print(angles_w_avg)
+
+# Plot the weighted average line
+ax.plot([0, weighted_average_angle], [0, ax.get_ylim()[1]], color='red', linewidth=2)
+ax1.plot([0, weighted_average_angle1], [0, ax1.get_ylim()[1]], color='blue', linewidth=2)
+
+ax.set_title('weighted with all vectors together')
+ax1.set_title('weighed with each wave_id\'s average then averaged together')
+
+plt.show()
+
+#Mouse Names
+'''
+119_2
+132_1
+132_2
+328A_3
+E2
+L1
+L3
+'''
