ddd316973fc3528c92c372641c75d53d8d0bf1a3
ambijani <a.bijani@wustl.edu>
2023-07-31 15:04:37 -0700

    multiple and single file filter functionality

--- lost.py
+++ lost.py
@@ -5,16 +5,18 @@ from pathlib import Path
 import argparse
 
 # Function to parse input
-def parse_wave_ids(input_str):
+def parse_wave_ids(wave_ids_input):
     wave_ids = []
-    for part in input_str.split(','):
-        if '-' in part:
-            start, end = part.split('-')[0], part.split('-')[1]
-            wave_ids.extend(range(int(start), int(end) + 1))
-        else:
-            wave_ids.append(int(part))
+    if wave_ids_input:
+        for part in wave_ids_input.split(','):
+            if '-' in part:
+                start, end = part.split('-')[0], part.split('-')[1]
+                wave_ids.extend(range(int(start), int(end) + 1))
+            else:
+                wave_ids.append(int(part))
+    print(max(wave_ids))
     return wave_ids
-    
+  
 # Argument parser 
 parser = argparse.ArgumentParser(
     prog='Polar Direction Plotter',
@@ -33,11 +35,21 @@ avg_y_normalized = []
 
 data = []
 
+#Loop through input of 'filename:wave_ids' and check for specific waves or all waves
 for entry in args.filename_wave_ids:
-    filename, wave_ids_input = entry.split(':')
-    wave_ids = parse_wave_ids(wave_ids_input)
-    data.append({'filename': filename, 'wave_ids': wave_ids})
+    if ':' in entry:    
+        filename, wave_ids_input = entry.split(':')
+        wave_ids = parse_wave_ids(wave_ids_input)
+        data.append({'filename': filename, 'wave_ids': wave_ids})
+    else:
+        filename = entry
+        df = pd.read_csv(Path(f"D:\\ANESTH_{filename}_KX_16\\stage05_channel-wave_characterization\\direction_local\\wavefronts_direction_local.csv"))
+        last_wave_id = df['wavefronts_id'].max()
+        wave_ids_input = f'1-{last_wave_id}'  # Set wave_ids_input as a range from 1 to last_wave_id
+        wave_ids = parse_wave_ids(wave_ids_input)
+        data.append({'filename': filename, 'wave_ids': wave_ids})
 
+#Loop and extract data to normalize and calculate average values
 for entry in data:
     filename = entry['filename']
     wave_ids = entry['wave_ids']
@@ -71,7 +83,7 @@ for entry in data:
 
 # Calculate angles using arctan2
 angles = np.arctan2(all_directionY, all_directionX)
-angles_w_avg = np.arctan2(avg_y_normalized, avg_x_normalized)
+angles_norm = np.arctan2(avg_y_normalized, avg_x_normalized)
 
 # Calculate the weighted average angle
 weighted_average_angle = np.arctan2(np.average(all_directionY, axis=0), 
@@ -81,19 +93,17 @@ weighted_average_angle1 = np.arctan2(np.average(avg_y_normalized, axis=0),
 print('weighted with all vectors together', weighted_average_angle)
 print('waves normalized individually then normalized together', weighted_average_angle1)
 
-# Create a polar histogram
+# Create a polar histogram each method
 fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
 ax.hist(angles, bins=36, range=(-np.pi, np.pi), density=True)
+ax.set_title('all vectors normalized together')
 
 fig1, ax1 = plt.subplots(subplot_kw={'projection': 'polar'})
-ax1.hist(angles_w_avg, bins=36, range=(-np.pi, np.pi), density=True)
-#print(wave_ids, np.rad2deg(angles_w_avg))
+ax1.hist(angles_norm, bins=36, range=(-np.pi, np.pi), density=True)
+ax1.set_title('normalized vectors by each wave individually then together')
 
 # Plot the weighted average line
 ax.plot([0, weighted_average_angle], [0, ax.get_ylim()[1]], color='red', linewidth=2)
 ax1.plot([0, weighted_average_angle1], [0, ax1.get_ylim()[1]], color='black', linewidth=2)
-
-ax.set_title('weighted with all vectors together')
-ax1.set_title('waves normalized individually then normalized together')
 
 plt.show()
